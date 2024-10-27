0484b3b31402225817bcc08b508c309e2109231f
Giovanny <giovannyxdd@gmail.com>
2024-10-27 16:31:46 -0300

    Initial commit

---
+++ src/main/java/com/example/apirecetas/ApirecetasApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirecetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirecetasApplication.class, args);
+	}
+
+}
