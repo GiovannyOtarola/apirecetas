b818e459984aa01f6ad14b6434b3bbe5ebe1b248
O009920 <mfuentes@forum.cl>
2024-10-27 17:37:05 -0300

    avance desarrollo

--- src/main/java/com/example/apirecetas/repository/UserRepository.java
+++ src/main/java/com/example/apirecetas/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.apirecetas.repository;
 import com.example.apirecetas.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository   extends CrudRepository {
+public interface UserRepository extends CrudRepository<Long,User> {
 
     User findByUsername(String username);
 }
