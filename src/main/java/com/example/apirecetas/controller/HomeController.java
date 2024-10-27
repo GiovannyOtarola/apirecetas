d75b13eccf9bfeec88848920241c411cd644284f
O009920 <mfuentes@forum.cl>
2024-10-27 18:14:57 -0300

    avance desarrollo

--- src/main/java/com/example/apirecetas/controller/HomeController.java
+++
@@ -1,4 +0,0 @@
-package com.example.apirecetas.controller;
-
-public class HomeController {
-}
--- src/main/java/com/example/apirecetas/repository/UserRepository.java
+++ src/main/java/com/example/apirecetas/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.apirecetas.repository;
 import com.example.apirecetas.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<Long,User> {
+public interface UserRepository extends CrudRepository<User,Integer> {
 
     User findByUsername(String username);
 }
